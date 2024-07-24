b6ffd8f54d1e576d9fbd7066bd5bdffb7b64d243
umair379 <160377759+umair379@users.noreply.github.com>
2024-07-24 23:22:22 +0500

    typescript number guessing machine

---
+++ index.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+// 1) computer will generate a random number
+
+// 2) user input for guessing number
+
+// 3) compare user input with computer generated number and show result
+
+// const randomNumber = 13;
+const randomNumber = Math.floor(Math.random() *6 + 1);
+
+const answers = await inquirer.prompt([
+    {
+        name: "userguessingnumber",
+        type: "number",
+        message: "please guess a number between (1 to 6)",
+    },
+]);
+
+if (answers.userguessingnumber === randomNumber) {
+    console.log("congratulation! you guessed right number.");
+} else {
+    console.log("you guessed wrong number");
+}
